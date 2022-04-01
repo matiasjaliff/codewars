82c310a5c3b419e574f1aa71da11cd4d57abe357
Matias Jaliff <matiasjaliff@gmail.com>
2022-04-01 15:38:05 -0300

    Actualización

---
+++ 12 - Human Readable Time/script.js
@@ -0,0 +1,22 @@
+// Human Readable Time
+
+// Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+// HH = hours, padded to 2 digits, range: 00 - 99
+// MM = minutes, padded to 2 digits, range: 00 - 59
+// SS = seconds, padded to 2 digits, range: 00 - 59
+// The maximum time never exceeds 359999 (99:59:59)
+
+// You can find some examples in the test fixtures.
+
+function humanReadable(seconds) {
+    if (seconds > 359999) return '99:59:59';
+    let HH = '0';
+    let MM = '0';
+    let SS = '0'; 
+    HH += Math.floor(seconds / 3600);
+    seconds = seconds % 3600;
+    MM += Math.floor(seconds / 60);
+    SS += seconds % 60;
+    return `${HH.slice(-2)}:${MM.slice(-2)}:${SS.slice(-2)}`;
+}
