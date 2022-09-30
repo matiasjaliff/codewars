ebb1a7a47385d7ad525984b241e2c3286a1b568a
Matias Jaliff <matiasjaliff@gmail.com>
2022-09-30 12:28:08 -0300

    "solved: kata 003"

---
+++ typescript/003-8kyu-lost-without-a-map/lost-without-a-map.js
@@ -0,0 +1,8 @@
+// 8 kyu
+// Beginner - Lost Without a Map
+// Given an array of integers, return a new array with each value doubled.
+// For example:
+// [1, 2, 3] --> [2, 4, 6]
+function maps(arr) {
+    return arr.map(function (element) { return element * 2; });
+}
